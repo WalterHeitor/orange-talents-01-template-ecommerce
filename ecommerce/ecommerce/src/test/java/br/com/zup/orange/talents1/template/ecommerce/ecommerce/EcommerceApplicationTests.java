2f9ad20684186e2682958a8923d1c86c3300eb74
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-29 13:46:28 -0300

    Create EcommerceApplicationTests.java

---
+++ ecommerce/ecommerce/src/test/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.orange.talents1.template.ecommerce.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
