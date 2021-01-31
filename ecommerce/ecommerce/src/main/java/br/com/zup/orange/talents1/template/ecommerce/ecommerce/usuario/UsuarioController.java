8fc736327d687cbb0452459cb4555f8c1e134c52
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-31 11:02:42 -0300

    Cadastro de Ususario

--- ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/usuario/UsuarioController.java
+++ ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/usuario/UsuarioController.java
@@ -1,5 +1,23 @@
 package br.com.zup.orange.talents1.template.ecommerce.ecommerce.usuario;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<?>salvar(@RequestBody @Valid UsuarioRequest usuarioRequest){
+		return ResponseEntity.ok(usuarioRequest.toString());
+		
+	}
 
 }
---
+++ ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/usuario/UsuarioRequest.java
@@ -0,0 +1,64 @@
+package br.com.zup.orange.talents1.template.ecommerce.ecommerce.usuario;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.sun.istack.NotNull;
+
+public class UsuarioRequest {
+	
+	@NotBlank
+	@Email
+	private String login;
+	@NotBlank 
+	@Size(min = 6, message = "A senha deve ter no minimo {min} "
+			+ "caracteres. Voçe digitou: ${validateValue} . ")
+	private String senha;
+	@NotNull
+	@Past(message = "A data deve estar no passado")
+	@JsonFormat(pattern = "dd/MM/yyyy", shape = Shape.STRING)
+	private LocalDate dateCadastro;
+	
+	
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public LocalDate getDateCadastro() {
+		return dateCadastro;
+	}
+
+	public UsuarioRequest() {	}
+
+	public UsuarioRequest(@NotBlank @Email String login, @NotBlank @Size String senha,
+			@Past LocalDate dateCadastro) {
+		super();
+		this.login = login;
+		this.senha = senha;
+		this.dateCadastro = dateCadastro;
+	}
+	
+	public void setDateCadastro(LocalDate dateCadastro) {
+		this.dateCadastro = dateCadastro;
+	}
+
+	@Override
+	public String toString() {
+		return "UsuarioRequest [login=" + login + ", senha=" + senha + ", dateCadastro=" + dateCadastro + "]";
+	}
+	
+	
+	
+
+}
