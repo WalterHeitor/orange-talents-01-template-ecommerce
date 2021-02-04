b8287562bbbcce4e9dfc005766359052d891d75a
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-02-04 09:53:22 -0300

    Create Categoria.java

---
+++ ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/categoria/Categoria.java
@@ -0,0 +1,42 @@
+package br.com.zup.orange.talents1.template.ecommerce.ecommerce.categoria;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Categoria {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Column(nullable = false, unique = true)
+	private String nome;
+	@ManyToOne	
+	private Categoria categoriaMae;
+	
+	@Deprecated
+	public Categoria() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Categoria(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public void setCategoriaMae(Categoria categoriaMae) {
+		this.categoriaMae = categoriaMae;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + ", categoriaMae=" + categoriaMae + "]";
+	}
+	
+}
