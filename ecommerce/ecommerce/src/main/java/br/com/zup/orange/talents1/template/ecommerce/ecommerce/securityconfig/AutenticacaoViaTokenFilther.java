1b0509d98f8fe4a2ad65bb5f65a196ab745e96d3
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-02-07 14:49:12 -0300

    Update AutenticacaoViaTokenFilther.java

--- ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/securityconfig/AutenticacaoViaTokenFilther.java
+++ ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/securityconfig/AutenticacaoViaTokenFilther.java
@@ -13,28 +13,28 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import br.com.zup.orange.talents1.template.ecommerce.ecommerce.usuario.Usuario;
 import br.com.zup.orange.talents1.template.ecommerce.ecommerce.usuario.UsuarioRepository;
-
+//carga intricica 6
 public class AutenticacaoViaTokenFilther extends OncePerRequestFilter{
 	
 	private TokenService tokenService;
 	private UsuarioRepository usuarioRepository;
-
+//1
 	public AutenticacaoViaTokenFilther(TokenService tokenService, UsuarioRepository usuarioRepository) {
 		this.tokenService = tokenService;
 		this.usuarioRepository = usuarioRepository;
 	}
-
+//2
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		String token = recuperarToken(request);
 		boolean valido = tokenService.isvalido(token);
-		if(valido) {
+		if(valido) {		//3
 			autenticarUsuario(token);
 		}
 		filterChain.doFilter(request, response);
 	}
-
+//4
 	private void autenticarUsuario(String token) {
 		Long idUsuario = tokenService.getIdUsuario(token);
 		Usuario usuario = usuarioRepository.findById(idUsuario).get();
@@ -43,10 +43,10 @@ public class AutenticacaoViaTokenFilther extends OncePerRequestFilter{
 		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 		
 	}
-
+//5
 	private String recuperarToken(HttpServletRequest request) {
 		String token = request.getHeader("Authorization");
-		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) { //6
 			return null;	
 		}
 		return token.substring(7, token.length());
