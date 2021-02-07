9b49b5b2fa0e42ea5a855195f47d26f0282d612f
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-02-07 14:49:31 -0300

    Create CaracteristicaProduto.java

---
+++ ecommerce/ecommerce/src/main/java/br/com/zup/orange/talents1/template/ecommerce/ecommerce/produto/CaracteristicaProduto.java
@@ -0,0 +1,46 @@
+package br.com.zup.orange.talents1.template.ecommerce.ecommerce.produto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+@Entity
+public class CaracteristicaProduto {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private @NotBlank String nome;
+	private @NotBlank String descricao;
+	@ManyToOne
+	private @NotNull @Valid Produto produto;
+
+	public CaracteristicaProduto(@NotBlank String nome, @NotBlank String descricao, @NotNull @Valid Produto produto) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.produto = produto;
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	@Override
+	public String toString() {
+		return "CaracteristicaProduto [nome=" + nome + ", descricao=" + descricao + "]";
+	}
+
+	
+}
